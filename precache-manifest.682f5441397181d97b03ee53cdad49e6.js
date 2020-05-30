3d9491e494ee285cdd6d9543af9f3621f2d5f515
Manoj <manoj.gudivada1@gmail.com>
2020-05-30 17:27:55 +0530

    Updates

---
+++ precache-manifest.682f5441397181d97b03ee53cdad49e6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d9c462a5d072481efe29be1b5287f188",
+    "url": "/CoronaTracker/index.html"
+  },
+  {
+    "revision": "9b3007a57f35574ae67b",
+    "url": "/CoronaTracker/static/css/main.036ad8ac.chunk.css"
+  },
+  {
+    "revision": "ee6859ab404fef377366",
+    "url": "/CoronaTracker/static/js/2.8fd793d1.chunk.js"
+  },
+  {
+    "revision": "44e39e61fb6a0450f7c8014318774752",
+    "url": "/CoronaTracker/static/js/2.8fd793d1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9b3007a57f35574ae67b",
+    "url": "/CoronaTracker/static/js/main.672eb84c.chunk.js"
+  },
+  {
+    "revision": "597c64ed36e2afa5fb37",
+    "url": "/CoronaTracker/static/js/runtime-main.c2155eb9.js"
+  }
+]);
